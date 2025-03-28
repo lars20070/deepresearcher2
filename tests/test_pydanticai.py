06714cd50c788ee44d20e2a2d27ea7ae1f683bc9
lars20070 <l.nilse@dunelm.org.uk>
2025-03-28 18:54:25 +0100

    one minimal PydanticAI test

--- tests/test_pydanticai.py
+++ tests/test_pydanticai.py
@@ -9,16 +9,34 @@ from deepresearcher2 import logger
 @pytest.mark.example
 @pytest.mark.paid
 @pytest.mark.asyncio
-async def test_basic_pydanticai(load_env: None) -> None:
+async def test_pydanticai_agent(load_env: None) -> None:
     """
-    Test basic pydanticai
+    Test the Agent() class with a cloud model
     """
-    logger.info("Testing basic pydanticai functionality.")
+    logger.info("Testing PydanticAI Agent() class with a cloud model")
 
     agent = Agent(
-        "google-gla:gemini-1.5-flash",
+        model="google-gla:gemini-1.5-flash",
         system_prompt="Be concise, reply with one sentence.",
     )
 
     result = await agent.run('Where does "hello world" come from?')
-    logger.info(f"Result from agent: {result.data}")
+    logger.debug(f"Result from agent: {result.data}")
+
+
+@pytest.mark.example
+@pytest.mark.paid
+@pytest.mark.asyncio
+async def test_pydanticai_agent_2(load_env: None) -> None:
+    """
+    Test the Agent() class with a cloud model
+    """
+    logger.info("Testing PydanticAI Agent() class with a cloud model")
+
+    agent = Agent(
+        model="google-gla:gemini-1.5-flash",
+        system_prompt="Be concise, reply with one sentence.",
+    )
+
+    result = await agent.run('Where does "hello world" come from?')
+    logger.debug(f"Result from agent: {result.data}")
