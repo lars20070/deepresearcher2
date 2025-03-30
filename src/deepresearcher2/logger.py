4af3f327fc475f72f2e46b850e8b4f58b94ff45f
lars20070 <l.nilse@dunelm.org.uk>
2025-03-30 11:24:13 +0200

    configure and test Logfire

--- src/deepresearcher2/logger.py
+++ src/deepresearcher2/logger.py
@@ -1,4 +1,20 @@
+import os
+
+import logfire
+from dotenv import load_dotenv
 from loguru import logger
 
+# Configure Loguru
 logger.remove(0)  # Remove default console logger
-logger.add(__name__.split(".")[0] + ".log", rotation="500 MB")
+logger.add(
+    __name__.split(".")[0] + ".log",
+    rotation="500 MB",
+    level="DEBUG",
+)
+
+# Configure Logfire
+load_dotenv()
+logfire.configure(
+    token=os.environ.get("LOGFIRE_TOKEN"),
+    send_to_logfire=True,
+)
