5f702c3cc818a169145ee056f10fb95fd4edac02
lars20070 <l.nilse@dunelm.org.uk>
2025-03-30 13:00:59 +0200

    Logfire as sink for Loguru

--- src/deepresearcher2/logger.py
+++ src/deepresearcher2/logger.py
@@ -4,6 +4,13 @@ import logfire
 from dotenv import load_dotenv
 from loguru import logger
 
+# Configure Logfire
+load_dotenv()
+logfire.configure(
+    token=os.environ.get("LOGFIRE_TOKEN"),
+    send_to_logfire=True,
+)
+
 # Configure Loguru
 logger.remove(0)  # Remove default console logger
 logger.add(
@@ -12,9 +19,6 @@ logger.add(
     level="DEBUG",
 )
 
-# Configure Logfire
-load_dotenv()
-logfire.configure(
-    token=os.environ.get("LOGFIRE_TOKEN"),
-    send_to_logfire=True,
-)
+# Logfire as sink for Loguru
+# i.e. emit a Logfire log for every Loguru log
+logger.configure(handlers=[logfire.loguru_handler()])
