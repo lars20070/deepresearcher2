9ebaff25cdadc72cb25ec8ff010584ad555954c4
lars20070 <l.nilse@dunelm.org.uk>
2025-04-02 17:12:58 +0200

    execute_code() tool works.

--- src/deepresearcher2/workflow.py
+++ src/deepresearcher2/workflow.py
@@ -4,21 +4,11 @@ import logfire
 import rizaio
 from dotenv import load_dotenv
 from pydantic_ai import Agent
+from pydantic_ai.exceptions import ModelRetry
 from pydantic_ai.models.openai import OpenAIModel
 from pydantic_ai.providers.openai import OpenAIProvider
 
 from deepresearcher2 import logger
-
-
-# Simulate code execution
-def execute_code(code: str) -> str:
-    load_dotenv()
-
-    print(f"Executing code: {code}")
-    riza = rizaio.Riza()
-    result = riza.command.exec(language="PYTHON", code=code)
-
-    return result.stdout
 
 
 def riza_example() -> None:
@@ -33,6 +23,8 @@ def riza_example() -> None:
     logfire.info("Starting Rizza example.")
 
     model = "llama3.3"
+    # model = "qwen2.5:72b"
+    # model = "qwq:32b"
     ollama_model = OpenAIModel(
         model_name=model,
         provider=OpenAIProvider(
@@ -45,11 +37,44 @@ def riza_example() -> None:
         system_prompt="You are a helpful assistant.",
     )
 
+    @agent.tool_plain
+    def execute_code(code: str) -> str:
+        """
+        Execute Python code
+
+        Use print() to write the output of your code to stdout.
+        Use only the Python standard library and build in modules.
+        For example, do not use pandas, but you can use csv.
+        Use httpx to make http requests.
+
+        Args:
+            code (str): The code to execute.
+
+        Returns:
+            str: The output of the code execution.
+        """
+        load_dotenv()
+
+        print(f"Executing code:\n{code}")
+        riza = rizaio.Riza()
+        result = riza.command.exec(
+            language="PYTHON",
+            code=code,
+        )
+
+        if result.exit_code != 0:
+            raise ModelRetry(result.stderr)
+        if result.stdout == "":
+            raise ModelRetry("Code executed successfully, but no output was returned. Ensure your code includes print statements for output.")
+
+        print(f"Execution output:\n{result.stdout}")
+        return result.stdout
+
     user_message = "What is the capital of France?"
     result = agent.run_sync(user_message)
 
-    while user_message != "quit":
-        print(f"Result: {result.data}")
+    while user_message not in {"exit", "quit"}:
+        print(f"Result:\n{result.data}")
         user_message = input("> ")
         result = agent.run_sync(
             user_message,
@@ -68,8 +93,7 @@ def main() -> None:
     logger.info("Starting main function.")
     logfire.info("Starting main function.")
 
-    # riza_example()
-    print(execute_code("print('Hello, world!')"))
+    riza_example()
 
 
 if __name__ == "__main__":
