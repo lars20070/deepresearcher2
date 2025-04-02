100556b9204708a07c5246522da46273004945a2
lars20070 <l.nilse@dunelm.org.uk>
2025-04-02 15:13:17 +0200

    What is the capital of France? works.

--- src/deepresearcher2/workflow.py
+++ src/deepresearcher2/workflow.py
@@ -1,8 +1,41 @@
 #!/usr/bin/env python3
 
 import logfire
+from pydantic_ai import Agent
+from pydantic_ai.models.openai import OpenAIModel
+from pydantic_ai.providers.openai import OpenAIProvider
 
 from deepresearcher2 import logger
+
+
+def riza_example() -> None:
+    """
+    Example by Riza team. https://riza.io
+    https://youtu.be/2FsN4f4z2CY
+
+    Returns:
+        None
+    """
+
+    logfire.info("Starting Rizza example.")
+
+    model = "llama3.3"
+    ollama_model = OpenAIModel(
+        model_name=model,
+        provider=OpenAIProvider(
+            base_url="http://localhost:11434/v1",
+        ),
+    )
+
+    agent = Agent(
+        ollama_model,
+        system_prompt="You are a helpful assistant.",
+    )
+
+    user_message = "What is the capital of France?"
+    result = agent.run_sync(user_message)
+
+    logger.info(f"Result: {result.data}")
 
 
 def main() -> None:
@@ -15,6 +48,8 @@ def main() -> None:
 
     logger.info("Starting main function.")
     logfire.info("Starting main function.")
+
+    riza_example()
 
 
 if __name__ == "__main__":
