a28c4617c57809696423c5d9a9b7ee634f54ed93
lars20070 <l.nilse@dunelm.org.uk>
2025-04-14 18:44:48 +0200

    data classes for email example

--- src/deepresearcher2/workflow.py
+++ src/deepresearcher2/workflow.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 import asyncio
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 from dotenv import load_dotenv
+from pydantic import BaseModel, EmailStr
 from pydantic_ai import Agent
 from pydantic_ai.mcp import MCPServerStdio
+from pydantic_ai.messages import ModelMessage
 from pydantic_ai.models.openai import OpenAIModel
 from pydantic_ai.providers.openai import OpenAIProvider
 from pydantic_graph import BaseNode, End, Graph, GraphRunContext
@@ -78,27 +80,27 @@ async def deepresearch() -> None:
 @dataclass
 class NodeA(BaseNode[int]):
     """
-    Node A for start.
+    Pass track number on.
     """
 
     track_number: int = 0
 
     async def run(self, ctx: GraphRunContext) -> BaseNode:
-        logger.info("Running Node A.")
+        logger.debug("Running Node A.")
         return NodeB(self.track_number)
 
 
 @dataclass
 class NodeB(BaseNode[int]):
     """
-    Node B for decision.
+    Decision node.
     """
 
     track_number: int = 0
 
     async def run(self, ctx: GraphRunContext) -> BaseNode | End:
-        logger.info("Running Node B.")
-        if self.track_number == 1:
+        logger.debug("Running Node B.")
+        if self.track_number > 5:
             return End(f"Stop at Node B with track number {self.track_number}")
         else:
             return NodeC(self.track_number)
@@ -107,14 +109,14 @@ class NodeB(BaseNode[int]):
 @dataclass
 class NodeC(BaseNode[int]):
     """
-    Node C optional.
+    Not always executed.
     """
 
     track_number: int = 0
 
     async def run(self, ctx: GraphRunContext) -> End:
         logger.info("Running Node C.")
-        return End(f"Value to be returned at Node C: {self.track_number}")
+        return End(f"Stop at Node C with track number {self.track_number}")
 
 
 async def deepresearch_2() -> None:
@@ -127,8 +129,56 @@ async def deepresearch_2() -> None:
     graph = Graph(nodes=[NodeA, NodeB, NodeC])
 
     # Run the agent graph
-    result = await graph.run(start_node=NodeA(track_number=1))
+    result = await graph.run(start_node=NodeA(track_number=6))
     logger.debug(f"Result: {result.output}")
+
+
+@dataclass
+class User:
+    name: str
+    email: EmailStr
+
+
+@dataclass
+class State:
+    user: User
+    write_agent_messages: list[ModelMessage] = field(default_factory=list)
+
+
+@dataclass
+class Email:
+    subject: str
+    body: str
+
+
+class EmailRequiresWrite(BaseModel):
+    feedback: str
+
+
+class EmailOk(BaseModel):
+    pass
+
+
+async def deepresearch_3() -> None:
+    ollama_model = OpenAIModel(
+        model_name="llama3.3",
+        provider=OpenAIProvider(base_url="http://localhost:11434/v1"),
+    )
+
+    email_writer_agent = Agent(
+        model=ollama_model,
+        result_type=Email,
+        system_prompt="Write a welcome email to the people who subscribed to my blog.",
+    )
+
+    feedback_agnet = Agent(
+        model=ollama_model,
+        result_type=EmailRequiresWrite | EmailOk,
+        system_prompt="Review the email and provide feedback. Email must reference the users specific interests.",
+    )
+
+    logger.debug(email_writer_agent)
+    logger.debug(feedback_agnet)
 
 
 def main() -> None:
@@ -137,7 +187,7 @@ def main() -> None:
     """
 
     logger.info("Starting deep research.")
-    asyncio.run(deepresearch_2())
+    asyncio.run(deepresearch_3())
 
 
 if __name__ == "__main__":
