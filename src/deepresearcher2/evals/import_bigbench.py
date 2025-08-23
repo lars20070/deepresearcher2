1c7ff3cb03ab9a50b8228e7c2af174bc55f25db0
lars20070 <l.nilse@dunelm.org.uk>
2025-08-23 11:32:04 +0200

    restructure project for evals

---
+++ src/deepresearcher2/evals/import_bigbench.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+from __future__ import annotations as _annotations
+
+from deepresearcher2.logger import logger
+
+
+def main() -> None:
+    """
+    Main function containing the import workflow.
+    """
+
+    logger.info("Starting import workflow.")
+
+
+if __name__ == "__main__":
+    main()
