2831408ca64765d082e70da93cc462b370db6604
lars20070 <l.nilse@dunelm.org.uk>
2025-08-25 14:28:52 +0200

    skeleton for generate benchmark

---
+++ src/deepresearcher2/evals/generate.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+from deepresearcher2.logger import logger
+
+
+def generate_search_summaries() -> None:
+    logger.info("Generating search summaries.")
+
+
+def main() -> None:
+    generate_search_summaries()
+
+
+if __name__ == "__main__":
+    main()
