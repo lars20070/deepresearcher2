1ef58849993e54b9255c8abcef1c7f95c862ea74
lars20070 <l.nilse@dunelm.org.uk>
2025-08-26 09:29:55 +0200

    fix beautifulsoup unit test

--- tests/test_example.py
+++ tests/test_example.py
@@ -1038,9 +1038,13 @@ def test_beautifulsoup() -> None:
     """
 
     url = "https://en.wikipedia.org/wiki/Petrichor"
+    headers = {
+        "User-Agent": ("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"),
+        "Accept-Language": "en-US,en;q=0.9",
+    }
 
     # Read raw html
-    request = urllib.request.Request(url)
+    request = urllib.request.Request(url, headers=headers)
     response = urllib.request.urlopen(request)
     html = response.read()
     logger.debug(f"Raw html response:\n{html}")
