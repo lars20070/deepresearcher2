abfc21ce69b374967e23c02ccac1ea8a5e1a77fc
lars20070 <l.nilse@dunelm.org.uk>
2026-01-09 09:35:37 +0100

    decorator for timer

--- tests/evals/test_evals.py
+++ tests/evals/test_evals.py
@@ -202,7 +202,8 @@ async def test_adaptive_uncertainty_strategy(ice_cream_players: list[EvalPlayer]
 
 @pytest.mark.vcr()
 @pytest.mark.asyncio
-async def test_evaltournament_usecase(tmp_path: Path, timer_for_tests: None) -> None:
+@pytest.mark.usefixtures("timer_for_tests")
+async def test_evaltournament_usecase(tmp_path: Path) -> None:
     """
     Use case for EvalTournament, EvalGame and EvalPlayer classes.
 
