1d6b67e174225f46c67c2fb009aed352a1396ac0
lars20070 <l.nilse@dunelm.org.uk>
2026-01-16 11:46:41 +0100

    add further pytest plugin skeleton

---
+++ src/deepresearcher2/hooks.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+from _pytest.config import Config
+
+
+def pytest_recording_configure(config: Config) -> None:
+    pass  # pragma: no cover
--- src/deepresearcher2/plugin.py
+++ src/deepresearcher2/plugin.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
+from _pytest.config import Config, PytestPluginManager
 from _pytest.config.argparsing import Parser
+
+from .logger import logger
 
 ASSAY_MODES = ("evaluate", "new_baseline")
 
@@ -13,3 +16,28 @@ def pytest_addoption(parser: Parser) -> None:
         choices=ASSAY_MODES,
         help='Assay mode. Defaults to "evaluate".',
     )
+
+
+def pytest_configure(config: Config) -> None:
+    # config.addinivalue_line("markers", "vcr: Mark the test as using VCR.py.")
+    # config.addinivalue_line("markers", "block_network: Block network access except for VCR recording.")
+    # config.addinivalue_line("markers", "default_cassette: Override the default cassette name.")
+    # config.addinivalue_line(
+    #     "markers",
+    #     "allowed_hosts: List of regexes to match hosts to where connection must be allowed.",
+    # )
+    # network.install_pycurl_wrapper()
+
+    assay_mode = config.getoption("--assay-mode")
+    logger.debug(f"assay_mode={assay_mode}")
+
+    pass
+
+
+def pytest_unconfigure() -> None:
+    pass
+
+
+def pytest_addhooks(pluginmanager: PytestPluginManager) -> None:
+    # pluginmanager.add_hookspecs(hooks)
+    pass
